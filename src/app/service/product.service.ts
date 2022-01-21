01721b77b69d197a591aa79544077a217077cdc6
Judit Orosz <orosz.judit@gmail.com>
2022-01-21 13:35:04 +0100

    classes + service

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  jsonUrl: string = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  getAll() : Observable<Product[]> {
+    return this.http.get<Product[]>(this.jsonUrl);
+  }
+
+  getOne(id: string | number) : Observable<Product> {
+    return this.http.get<Product>(`${this.jsonUrl}/${id}`);
+  }
 }
