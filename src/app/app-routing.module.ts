82f6b05ad5de735244b9041c815d6c685e944737
Greg Molnar <molnar.gly@gmail.com>
2022-01-23 11:15:50 +0100

    some changes to start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Cat01Component } from './page/cat01/cat01.component';
-import { Cat02Component } from './page/cat02/cat02.component';
+import { Cat01Component } from './page/business/cat01.component';
+import { Cat02Component } from './page/casual/cat02.component';
 import { HomeComponent } from './page/home/home.component';
 
 const routes: Routes = [
@@ -20,11 +20,11 @@ const routes: Routes = [
   {
     path: '**',
     component: HomeComponent,
-  }
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
