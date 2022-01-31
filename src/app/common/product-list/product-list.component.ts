74a5e71639911d440fab284e5b07bde36de90168
Nmatse <atseniko@gmail.com>
2022-01-31 17:28:15 +0100

    sorterpipe

--- src/app/common/product-list/product-list.component.ts
+++ src/app/common/product-list/product-list.component.ts
@@ -17,12 +17,64 @@ export class ProductListComponent implements OnInit {
 
   sorterKey: string = '';
 
+  direction: number = -1;
+
+  directionPrice: number = 1;
+  directionName: number = 1;
+  directionStock: number = 1;
+  directionFeatured: number = 1;
+  directionActive: number = 1;
+
   constructor() {}
 
   ngOnInit(): void {}
 
   onSorterSelect(key: string): void {
     this.sorterKey = key;
+    if (this.sorterKey == 'price') {
+      this.directionPrice = 1;
+      this.direction = this.directionPrice
+    }
+    if (this.sorterKey == 'name') {
+      this.directionName = 1;
+      this.direction = this.directionName
+    }
+    if (this.sorterKey == 'stock') {
+      this.directionStock = 1;
+      this.direction = this.directionStock
+    }
+    if (this.sorterKey == 'featured') {
+      this.directionFeatured = 1;
+      this.direction = this.directionFeatured
+    }
+    if (this.sorterKey == 'active') {
+      this.directionActive = 1;
+      this.direction = this.directionActive
+    }
+  }
+
+  onBackSorterSelect(key: string): void {
+    this.sorterKey = key;
+    if (this.sorterKey == 'price') {
+      this.directionPrice = -1;
+      this.direction = this.directionPrice
+    }
+    if (this.sorterKey == 'name') {
+      this.directionName = -1;
+      this.direction = this.directionName
+    }
+    if (this.sorterKey == 'stock') {
+      this.directionStock = -1;
+      this.direction = this.directionStock
+    }
+    if (this.sorterKey == 'featured') {
+      this.directionFeatured = -1;
+      this.direction = this.directionFeatured
+    }
+    if (this.sorterKey == 'active') {
+      this.directionActive = -1;
+      this.direction = this.directionActive
+    }
   }
 
 }
