9894aaab872a6a50a38ec5acc81327356615a86b
Nmatse <atseniko@gmail.com>
2022-01-31 17:40:54 +0100

    sorterpipe2

--- src/app/pipe/sorter.pipe.ts
+++ src/app/pipe/sorter.pipe.ts
@@ -5,18 +5,27 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SorterPipe implements PipeTransform {
 
-  transform(value: any[], key: string): any[] {
+  transform(value: any[], key: string, direction: number): any[] {
     if (!Array.isArray(value) || !key) {
       return value
     }
     return value.sort((a, b) => {
+      if (direction === 1) {
       if (typeof a[key] === "number" && typeof b[key] === "number") {
         return a[key] - b[key]
       } else {
         return a[key].toString().toLowerCase().localeCompare(b[key].toString().toLowerCase())
       }
+    }
+    else if (direction === -1) {
+      if (typeof a[key] === "number" && typeof b[key] === "number") {
+        return b[key] - a[key]
+      } else {
+        return b[key].toString().toLowerCase().localeCompare(a[key].toString().toLowerCase())
+      }
+  }
+  })
 
-    })
   }
 
 }
